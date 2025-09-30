effe6a04da38d6218959107664794f0cc2772ff8
Srltas <kdm97912@gmail.com>
2025-09-30 18:51:21 +0900

    chore: add debug log

--- src/test/java/com/cmt/e2e/cmd/script/ScriptTest.java
+++ src/test/java/com/cmt/e2e/cmd/script/ScriptTest.java
@@ -1,10 +1,13 @@
 package com.cmt.e2e.cmd.script;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static com.cmt.e2e.support.Drivers.DB.CUBRID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.xmlunit.assertj.XmlAssert.assertThat;
 
 import com.cmt.e2e.support.CmtE2eTestBase;
@@ -39,10 +42,19 @@ public class ScriptTest extends CmtE2eTestBase {
         Files.lines(finalDbConf).forEach(System.out::println);
         System.out.println("---------------------");
 
+        System.out.println("[NET] host.docker.internal = " + InetAddress.getByName("host.docker.internal").getHostAddress());
+        try (var s = new Socket(CubridDemodbContainer.getHost(), CubridDemodbContainer.getMappedBrokerPort())) {
+            System.out.println("[NET] TCP OK to demodb");
+        } catch (Exception e) {
+            throw new AssertionError("[NET] TCP FAIL to demodb: " +
+                CubridDemodbContainer.getHost() + ":" + CubridDemodbContainer.getMappedBrokerPort(), e);
+        }
+
         String[] options = {"-s", "cubrid_source", "-t", "file_target", "-o", tempDir.toString()};
         var result = runner.script(options);
-
-        Assertions.assertEquals(0, result.exitCode(), "migration.sh failed. exit=" + result.exitCode() + "\n--- OUTPUT ---\n" + result.output());
+        System.out.printf("[RUN-RESULT] exit=%d, timedOut=%s%n%s%n",
+            result.exitCode(), result.timedOut(), result.output());
+        assertEquals(0, result.exitCode(), "script failed");
         Files.list(tempDir).forEach(p -> System.out.println("[TEMP] " + p));
 
         Path generatedScriptFile = testPaths.findGeneratedScriptFile(tempDir, "CUBRID_demodb_")
